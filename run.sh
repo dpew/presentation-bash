89dbe9c4eab229158e4c740db1996fe5dea97d4e
Douglas Pew [Spectric Labs] <dpew@spectric.com>
2024-06-02 10:35:06 -0400

    minor updates

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 mydir="$(realpath "$(dirname "$0")")"
 "$mydir/venv/bin/jupyter" nbextension enable rise --py --sys-prefix
-"$mydir/venv/bin/jupyter" --no-browser "$@"
+#"$mydir/venv/bin/jupyter" --no-browser "$@"
+"$mydir/venv/bin/jupyter" notebook
